6c981ce4a557b5da35706601df281560795f8a34
gouxubo <gouxubo@le.com>
2017-03-29 21:30:06 +0800

    头像文件夹

--- src/main/java/com/junlenet/mongodb/demo/util/Constant.java
+++ src/main/java/com/junlenet/mongodb/demo/util/Constant.java
@@ -2,5 +2,5 @@ package com.junlenet.mongodb.demo.util;
 
 public class Constant {
 	//头像文件夹
-	public static final String HEAD_PICTURE_PATH = "/Users/gouxubo/picture/head/";
+	public static final String HEAD_PICTURE_PATH = "/opt/apps/lad-server/picture/head/";
 }
