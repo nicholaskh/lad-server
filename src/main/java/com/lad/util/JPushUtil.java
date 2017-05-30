27af6d850971ac1111c3fd7b184d5b2c2624e75a
gouxubo <gouxubo@126.com>
2017-05-30 08:05:14 +0800

    极光推送，优化

--- src/main/java/com/lad/util/JPushUtil.java
+++ src/main/java/com/lad/util/JPushUtil.java
@@ -15,37 +15,63 @@ public class JPushUtil {
 	private static String MASTER_SECRET = "db587bc126abddf27e548ecc";
 	private static String APP_KEY = "d53e8d39d6df18e379bf5da4";
 
-	public static void push() {
-		// JPushClient jpush = new JPushClient(masterSecret, appKey);
+	public static void pushAll(String content) {
 		JPushClient jpushClient = new JPushClient(MASTER_SECRET, APP_KEY, null,
 				ClientConfig.getInstance());
-		 PushPayload payload = buildPushObject_all_alias_alert();
+		PushPayload payload = buildPushObject_all_alias_alert(content);
 		try {
-	        PushResult result = jpushClient.sendPush(payload);
-	        System.out.println("Got result - " + result);
+			PushResult result = jpushClient.sendPush(payload);
+			System.out.println("Got result - " + result);
 
-	    } catch (APIConnectionException e) {
-	        // Connection error, should retry later
-	    	System.out.println("Connection error, should retry later : "+ e.getMessage());
+		} catch (APIConnectionException e) {
+			System.out.println("Connection error, should retry later : "
+					+ e.getMessage());
 
-	    } catch (APIRequestException e) {
-	        // Should review the error, and fix the request
-	    	System.out.println("Should review the error, and fix the request : "+e.getMessage());
-	    	System.out.println("HTTP Status: " + e.getStatus());
-	    	System.out.println("Error Code: " + e.getErrorCode());
-	    	System.out.println("Error Message: " + e.getErrorMessage());
-	    }
+		} catch (APIRequestException e) {
+			System.out
+					.println("Should review the error, and fix the request : "
+							+ e.getMessage());
+			System.out.println("HTTP Status: " + e.getStatus());
+			System.out.println("Error Code: " + e.getErrorCode());
+			System.out.println("Error Message: " + e.getErrorMessage());
+		}
 	}
-	
-	public static PushPayload buildPushObject_all_alias_alert() {
-		 return PushPayload.newBuilder()
-	                .setPlatform(Platform.all())
-	                .setAudience(Audience.alias("laoyou"))
-	                .setNotification(Notification.alert("ALERT alias tiantian from xubo"))
-	                .build();
-    }
-	
-	public static void main(String [] args){
-		JPushUtil.push();
+
+	public static PushPayload buildPushObject_all_alias_alert(String content) {
+		return PushPayload.newBuilder().setPlatform(Platform.all())
+				.setNotification(Notification.alert(content)).build();
+	}
+
+	public static void pushTo(String content, String... alias) {
+		JPushClient jpushClient = new JPushClient(MASTER_SECRET, APP_KEY, null,
+				ClientConfig.getInstance());
+		PushPayload payload = buildPushObject_to_alias_alert(content, alias);
+		try {
+			PushResult result = jpushClient.sendPush(payload);
+			System.out.println("Got result - " + result);
+
+		} catch (APIConnectionException e) {
+			System.out.println("Connection error, should retry later : "
+					+ e.getMessage());
+
+		} catch (APIRequestException e) {
+			System.out
+					.println("Should review the error, and fix the request : "
+							+ e.getMessage());
+			System.out.println("HTTP Status: " + e.getStatus());
+			System.out.println("Error Code: " + e.getErrorCode());
+			System.out.println("Error Message: " + e.getErrorMessage());
+		}
+	}
+
+	public static PushPayload buildPushObject_to_alias_alert(String content,
+			String... alias) {
+		return PushPayload.newBuilder().setPlatform(Platform.all())
+				.setAudience(Audience.alias(alias))
+				.setNotification(Notification.alert(content)).build();
+	}
+
+	public static void main(String[] args) {
+		JPushUtil.pushAll("Test from B");
 	}
 }
