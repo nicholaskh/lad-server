14b64c3d9de2b284727dade932f408f9dbdf2156
dongensi <hi-flying@163.com>
2017-08-10 15:44:29 +0800

    反馈和举报帖子接口

--- src/main/java/com/lad/controller/FeedbackController.java
+++ src/main/java/com/lad/controller/FeedbackController.java
@@ -1,27 +1,29 @@
 package com.lad.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.lad.bo.FeedbackBo;
 import com.lad.bo.UserBo;
 import com.lad.service.IFeedbackService;
 import com.lad.service.IUserService;
 import com.lad.util.CommonUtil;
+import com.lad.util.Constant;
 import com.lad.util.ERRORCODE;
-
+import com.lad.util.MyException;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 @Controller
 @Scope("prototype")
@@ -37,22 +39,11 @@ public class FeedbackController extends BaseContorller {
 	@ResponseBody
 	public String isnert(String content, String contactInfo, String image,
 			HttpServletRequest request, HttpServletResponse response) {
-		HttpSession session = request.getSession();
-		if (session.isNew()) {
-			return CommonUtil.toErrorResult(
-					ERRORCODE.ACCOUNT_OFF_LINE.getIndex(),
-					ERRORCODE.ACCOUNT_OFF_LINE.getReason());
-		}
-		if (session.getAttribute("isLogin") == null) {
-			return CommonUtil.toErrorResult(
-					ERRORCODE.ACCOUNT_OFF_LINE.getIndex(),
-					ERRORCODE.ACCOUNT_OFF_LINE.getReason());
-		}
-		UserBo userBo = (UserBo) session.getAttribute("userBo");
-		if (userBo == null) {
-			return CommonUtil.toErrorResult(
-					ERRORCODE.ACCOUNT_OFF_LINE.getIndex(),
-					ERRORCODE.ACCOUNT_OFF_LINE.getReason());
+		UserBo userBo;
+		try {
+			userBo = checkSession(request, userService);
+		} catch (MyException e) {
+			return e.getMessage();
 		}
 		FeedbackBo feedbackBo = new FeedbackBo();
 		if (StringUtils.isEmpty(content)) {
@@ -62,10 +53,52 @@ public class FeedbackController extends BaseContorller {
 		feedbackBo.setContent(content);
 		feedbackBo.setContactInfo(contactInfo);
 		feedbackBo.setOwnerId(userBo.getId());
-		feedbackBo.setImage(image);
+		feedbackBo.getImages().add(image);
+		feedbackBo.setCreateuid(userBo.getId());
 		feedbackService.insert(feedbackBo);
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("ret", 0);
 		return JSONObject.fromObject(map).toString();
+	}
+
+	@RequestMapping("/note-tips-add")
+	@ResponseBody
+	public String noteTipsAdd(@RequestParam String module,
+						   @RequestParam(required = false)String subModule,
+						   @RequestParam String content,
+						   @RequestParam String targetId,
+						   @RequestParam String targetTitle,
+						   @RequestParam(required = false) MultipartFile[] pictures,
+						 HttpServletRequest request, HttpServletResponse response) {
+		UserBo userBo;
+		try {
+			userBo = checkSession(request, userService);
+		} catch (MyException e) {
+			return e.getMessage();
+		}
+		FeedbackBo feedbackBo = new FeedbackBo();
+		feedbackBo.setContent(content);
+		feedbackBo.setTargetId(targetId);
+		feedbackBo.setTargetTitle(targetTitle);
+		feedbackBo.setModule(module);
+		feedbackBo.setSubModule(subModule);
+		feedbackBo.setType(Constant.FEED_TIPS);
+		feedbackBo.setSubType(Constant.NOTE_TYPE);
+		feedbackBo.setCreateuid(userBo.getId());
+
+		LinkedList<String> images = feedbackBo.getImages();
+		String userId =  userBo.getId();
+		if (pictures != null) {
+			Long time = Calendar.getInstance().getTimeInMillis();
+			for (MultipartFile file : pictures) {
+				String fileName = userId + "-" + time + "-"
+						+ file.getOriginalFilename();
+				String path = CommonUtil.upload(file, Constant.FEEDBACK_PICTURE_PATH,
+						fileName, 0);
+				images.add(path);
+			}
+		}
+		feedbackService.insert(feedbackBo);
+		return Constant.COM_RESP;
 	}
 }
