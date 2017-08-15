a23ae42acc661d7d24b172704ee6398f29d52d83
dongensi <hi-flying@163.com>
2017-08-15 22:08:27 +0800

    用户更新位置接口

--- src/main/java/com/lad/controller/LocationController.java
+++ src/main/java/com/lad/controller/LocationController.java
@@ -1,61 +1,52 @@
 package com.lad.controller;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+import com.lad.bo.LocationBo;
+import com.lad.bo.UserBo;
+import com.lad.service.ILocationService;
+import com.lad.service.IUserService;
+import com.lad.util.CommonUtil;
+import com.lad.util.Constant;
+import com.lad.util.ERRORCODE;
+import com.lad.vo.UserVo;
 import net.sf.json.JSONObject;
-
 import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.lad.bo.LocationBo;
-import com.lad.bo.UserBo;
-import com.lad.service.ILocationService;
-import com.lad.service.IUserService;
-import com.lad.util.CommonUtil;
-import com.lad.util.ERRORCODE;
-import com.lad.vo.UserVo;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.*;
 
 @Controller
 @RequestMapping("location")
 public class LocationController extends BaseContorller {
-	
+
 	@Autowired
 	private ILocationService locationService;
 	@Autowired
 	private IUserService userService;
-	
+
 	@RequestMapping("/near")
 	@ResponseBody
-	public String near(double px, double py, HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
+	public String near(double px, double py, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		HttpSession session = request.getSession();
 		if (session.isNew()) {
-			return CommonUtil.toErrorResult(ERRORCODE.ACCOUNT_OFF_LINE.getIndex(),
-					ERRORCODE.ACCOUNT_OFF_LINE.getReason());
+			return CommonUtil.toErrorResult(ERRORCODE.ACCOUNT_OFF_LINE.getIndex(), ERRORCODE.ACCOUNT_OFF_LINE.getReason());
 		}
 		if (session.getAttribute("isLogin") == null) {
-			return CommonUtil.toErrorResult(ERRORCODE.ACCOUNT_OFF_LINE.getIndex(),
-					ERRORCODE.ACCOUNT_OFF_LINE.getReason());
+			return CommonUtil.toErrorResult(ERRORCODE.ACCOUNT_OFF_LINE.getIndex(), ERRORCODE.ACCOUNT_OFF_LINE.getReason());
 		}
 		UserBo userBo = (UserBo) session.getAttribute("userBo");
 		if (userBo == null) {
-			return CommonUtil.toErrorResult(ERRORCODE.ACCOUNT_OFF_LINE.getIndex(),
-					ERRORCODE.ACCOUNT_OFF_LINE.getReason());
+			return CommonUtil.toErrorResult(ERRORCODE.ACCOUNT_OFF_LINE.getIndex(), ERRORCODE.ACCOUNT_OFF_LINE.getReason());
 		}
 		userBo = userService.getUser(userBo.getId());
 		List<LocationBo> locationBoList = locationService.findCircleNear(px, py, 5000);
 		List<UserVo> list = new LinkedList<UserVo>();
-		for(LocationBo bo : locationBoList){
+		for (LocationBo bo : locationBoList) {
 			String userid = bo.getUserid();
 			UserBo temp = userService.getUser(userid);
 			UserVo vo = new UserVo();
@@ -66,5 +57,32 @@ public class LocationController extends BaseContorller {
 		map.put("ret", 0);
 		map.put("userList", list);
 		return JSONObject.fromObject(map).toString();
+	}
+
+
+	@RequestMapping("/update")
+	@ResponseBody
+	public String updateLocation(double px, double py, String phone,
+								 HttpServletRequest request, HttpServletResponse response) {
+		UserBo userBo = userService.getUserByPhone(phone);
+		if (null != userBo) {
+			LocationBo locationBo  = locationService.getLocationBoByUserid(userBo.getId());
+			double[] postion = new double[]{px, py};
+			if (null == locationBo) {
+				locationBo = new LocationBo();
+				locationBo.setUserid(userBo.getId());
+				locationBo.setPosition(postion);
+				locationBo = locationService.insertUserPoint(locationBo);
+			} else {
+				locationBo.setPosition(postion);
+				locationBo.setUpdateTime(new Date());
+				locationService.updateUserPoint(locationBo);
+			}
+			if (!locationBo.getId().equals(userBo.getLocationid())) {
+				userBo.setLocationid(locationBo.getId());
+				userService.updateLocation(phone, locationBo.getId());
+			}
+		}
+		return Constant.COM_RESP;
 	}
 }
