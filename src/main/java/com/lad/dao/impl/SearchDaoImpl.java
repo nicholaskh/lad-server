39227de4ae31a711fb85204b02c1cbd534d2d863
dongensi <hi-flying@163.com>
2017-09-05 20:06:29 +0800

    添加圈子热度搜索关键词

---
+++ src/main/java/com/lad/dao/impl/SearchDaoImpl.java
@@ -0,0 +1,65 @@
+package com.lad.dao.impl;
+
+import com.lad.bo.SearchBo;
+import com.lad.dao.ISearchDao;
+import com.mongodb.WriteResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 功能描述：
+ * Copyright: Copyright (c) 2017
+ * Version: 1.0
+ * Time:2017/9/4
+ */
+@Repository("searchDao")
+public class SearchDaoImpl implements ISearchDao {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Override
+    public SearchBo insert(SearchBo searchBo) {
+        mongoTemplate.insert(searchBo);
+        return searchBo;
+    }
+
+    @Override
+    public SearchBo findByKeyword(String keyword, int type) {
+        Query query = new Query();
+        query.addCriteria(new Criteria("keyword").is(keyword));
+        query.addCriteria(new Criteria("type").is(type));
+        return mongoTemplate.findOne(query,SearchBo.class);
+    }
+
+    @Override
+    public WriteResult update(String id) {
+        Query query = new Query();
+        query.addCriteria(new Criteria("_id").is(id));
+        Update update = new Update();
+        update.inc("times", 1);
+        return mongoTemplate.updateFirst(query, update, SearchBo.class);
+    }
+
+    @Override
+    public WriteResult delete(String id) {
+        return null;
+    }
+
+    @Override
+    public List<SearchBo> findByTimes(int type) {
+        Query query = new Query();
+        query.with(new Sort(new Sort.Order(Sort.Direction.DESC, "times")));
+        //圈子
+        query.addCriteria(new Criteria("type").is(type));
+        query.limit(12);
+        return mongoTemplate.find(query, SearchBo.class);
+    }
+}
