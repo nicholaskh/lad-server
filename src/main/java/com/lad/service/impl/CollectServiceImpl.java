3ca068482c51d689826a5a14eb1f1400e91482b7
dongensi <hi-flying@163.com>
2017-09-12 14:24:22 +0800

    修改收藏接口

--- src/main/java/com/lad/service/impl/CollectServiceImpl.java
+++ src/main/java/com/lad/service/impl/CollectServiceImpl.java
@@ -9,32 +9,32 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service("collecService")
+@Service("collectService")
 public class CollectServiceImpl implements ICollectService {
 	
 	@Autowired
-	private CollectDao collectionDao;
+	private CollectDao collectDao;
 	
 	
 	@Override
 	public CollectBo saveChat(CollectBo chatinfo) {
-		return collectionDao.save(chatinfo);
+		return collectDao.save(chatinfo);
 	}
 
 
 	@Override
 	public List<CollectBo> findMyChat(String userid) {
-		return collectionDao.findChatByUserid(userid);
+		return collectDao.findChatByUserid(userid);
 	}
 
 
 	@Override
 	public WriteResult delete(String chatId) {
-		return collectionDao.delete(chatId);
+		return collectDao.delete(chatId);
 	}
 
 	@Override
 	public List<CollectBo> findChatByUserid(String userid, String start_id, int limit) {
-		return collectionDao.findChatByUserid(userid, start_id, limit);
+		return collectDao.findChatByUserid(userid, start_id, limit);
 	}
 }
