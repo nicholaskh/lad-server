83c3d3254f667a584f85c61608be085f3c0e25c7
dongensi <hi-flying@163.com>
2017-09-18 19:59:10 +0800

    添加用户加入圈子记录，圈子分类获取列表添加用户加入记录

---
+++ src/main/java/com/lad/bo/CircleAddBo.java
@@ -0,0 +1,57 @@
+package com.lad.bo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+/**
+ * 功能描述： 圈子加入历史，是否加入过圈子
+ * Copyright: Copyright (c) 2017
+ * Version: 1.0
+ * Time:2017/9/17
+ */
+@Document(collection = "circleAdd")
+public class CircleAddBo implements Serializable {
+
+    @Id
+    private String id;
+
+    private String userid;
+
+    private String circleid;
+
+    private int status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getCircleid() {
+        return circleid;
+    }
+
+    public void setCircleid(String circleid) {
+        this.circleid = circleid;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
