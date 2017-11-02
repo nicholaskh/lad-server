a95df5193a4bb11599a68d44c96bd2238a11d81b
dongensi <hi-flying@163.com>
2017-11-02 22:09:22 +0800

    添加广播vo

---
+++ src/main/java/com/lad/vo/BroadcastVo.java
@@ -0,0 +1,100 @@
+package com.lad.vo;
+
+/**
+ * 功能描述：
+ * Copyright: Copyright (c) 2017
+ * Version: 1.0
+ * Time:2017/10/4
+ */
+public class BroadcastVo extends BaseVo {
+
+    private String inforid;
+
+    private String sourceUrl;
+
+    private String title;
+
+    private String module;
+
+    private String play_times;
+
+    private String className;
+
+    private String source;
+
+    private String intro;
+
+    private String broadcast_url;
+
+    public String getInforid() {
+        return inforid;
+    }
+
+    public void setInforid(String inforid) {
+        this.inforid = inforid;
+    }
+
+    public String getSourceUrl() {
+        return sourceUrl;
+    }
+
+    public void setSourceUrl(String sourceUrl) {
+        this.sourceUrl = sourceUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    public String getPlay_times() {
+        return play_times;
+    }
+
+    public void setPlay_times(String play_times) {
+        this.play_times = play_times;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro;
+    }
+
+    public String getBroadcast_url() {
+        return broadcast_url;
+    }
+
+    public void setBroadcast_url(String broadcast_url) {
+        this.broadcast_url = broadcast_url;
+    }
+}
